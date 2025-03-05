3b811b5ddf768a517ed32b7a36ada8260fc6e5bf
davvvin <2372054@maranatha.ac.id>
2025-03-05 11:44:46 +0700

    Third Week Commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
+
+Route::get('/', function () {
+    return view('layouts.starter');
+});
+
+Route::get('/1', function () {
+    return view('demo.file1');
+});
+
+
+Route::get('/2',[DemoController::class,'index']);
+
+Route::get('/3', function () {
+    return view('demo.file3');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
